5b634a430607a949d5ff2c1921f882372fafc4c4
Heo-Jung <hjj5367@naver.com>
2020-07-10 12:52:59 +0900

    done

---
+++ build/precache-manifest.3c23a47cc85e2abdf55ee02554cc43b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb2968668a0b9e9f2300b3e2deecbd1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "63750dcf04a7f10034c9",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63750dcf04a7f10034c9",
+    "url": "/movie_app/static/js/main.1af18c77.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
